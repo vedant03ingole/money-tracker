17d582a9cb9360ce4f04b3fabf94f82f2c8c81a1
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 20:56:39 +0530

    daied down-checking connection

--- src/app/transaction/page.jsx
+++ src/app/transaction/page.jsx
@@ -1,52 +1,63 @@
-import { addTransaction } from '@/lib/actions'
+// import { addTransaction } from '@/lib/actions'
 
-const AddTransaction = () => {
-    return (
-        <>
-            <div>AddTransaction</div>
-            <div className="text-black">
-                <form action={addTransaction}>
+// const AddTransaction = () => {
+//     return (
+//         <>
+//             <div>AddTransaction</div>
+//             <div className="text-black">
+//                 <form action={addTransaction}>
 
-                    {/* Transaction type */}
-                    <select name="transactionType" defaultValue="transactionType" required>
-                        <option value="transactionType" disabled hidden>Transaction type</option>
-                        <option value="income">Income</option>
-                        <option value="expense">Expense</option>
-                        <option value="transfer">Transfer</option>
-                    </select>
+//                     {/* Transaction type */}
+//                     <select name="transactionType" defaultValue="transactionType" required>
+//                         <option value="transactionType" disabled hidden>Transaction type</option>
+//                         <option value="income">Income</option>
+//                         <option value="expense">Expense</option>
+//                         <option value="transfer">Transfer</option>
+//                     </select>
 
-                    {/* Category */}
-                    <select name="category" defaultValue="category" required>
-                        <option value="category" disabled hidden>Transaction Category</option>
-                        <option value="rent">Rent</option>
-                        <option value="mess">Mess</option>
-                        <option value="petrol">Petrol</option>
-                        <option value="food">Food</option>
-                        <option value="other">Other expenses</option>
-                    </select>
+//                     {/* Category */}
+//                     <select name="category" defaultValue="category" required>
+//                         <option value="category" disabled hidden>Transaction Category</option>
+//                         <option value="rent">Rent</option>
+//                         <option value="mess">Mess</option>
+//                         <option value="petrol">Petrol</option>
+//                         <option value="food">Food</option>
+//                         <option value="other">Other expenses</option>
+//                     </select>
 
-                    {/* Transaction Amount */}
-                    <input type="number" name="transactionAmount" required />
+//                     {/* Transaction Amount */}
+//                     <input type="number" name="transactionAmount" required />
 
-                    {/* Patment Method */}
-                    <select name="paymentMethod" defaultValue="paymentMethod">
-                        <option value="paymentMethod" disabled hidden>Payment method</option>
-                        <option value="online">Online</option>
-                        <option value="cash">Cash</option>
-                    </select>
+//                     {/* Patment Method */}
+//                     <select name="paymentMethod" defaultValue="paymentMethod">
+//                         <option value="paymentMethod" disabled hidden>Payment method</option>
+//                         <option value="online">Online</option>
+//                         <option value="cash">Cash</option>
+//                     </select>
 
-                    {/* transaction date */}
-                    <input type="date" name="transactionDate" required />
+//                     {/* transaction date */}
+//                     <input type="date" name="transactionDate" required />
 
-                    {/* Description */}
-                    <textarea name="description" >Description</textarea>
+//                     {/* Description */}
+//                     <textarea name="description" >Description</textarea>
 
 
-                    <button className="text-white" type="submit">Send</button>
-                </form>
-            </div>
-        </>
-    )
+//                     <button className="text-white" type="submit">Send</button>
+//                 </form>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default AddTransaction
+
+
+import React from 'react'
+
+const Transaction = () => {
+  return (
+    <div>Transaction</div>
+  )
 }
 
-export default AddTransaction
+export default Transaction
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,25 +1,25 @@
-import { Transaction } from "./schema"
-import { connectToDB } from "./utils"
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
+// import { Transaction } from "./schema"
+// import { connectToDB } from "./utils"
+// import { revalidatePath } from "next/cache"
+// import { redirect } from "next/navigation"
 
-export  const addTransaction = async (formData) => {
-    "use server"
-    const { username, transactionType, category, transactionAmount, paymentMethod, transactionDate, description } = Object.fromEntries(formData)
-    console.log("Hello", username, "your transaction type is", category)
+// export  const addTransaction = async (formData) => {
+//     "use server"
+//     const { username, transactionType, category, transactionAmount, paymentMethod, transactionDate, description } = Object.fromEntries(formData)
+//     console.log("Hello", username, "your transaction type is", category)
 
-    try {
-        connectToDB()
-        console.log("connected")
+//     try {
+//         connectToDB()
+//         console.log("connected")
 
-        const newTransaction = new Transaction({ username, transactionType, category, transactionAmount, paymentMethod, transactionDate, description })
-        await newTransaction.save()
+//         const newTransaction = new Transaction({ username, transactionType, category, transactionAmount, paymentMethod, transactionDate, description })
+//         await newTransaction.save()
 
-    } catch (error) {
-        console.log(error)
-        throw new Error("Failed to add transaction")
-    }
+//     } catch (error) {
+//         console.log(error)
+//         throw new Error("Failed to add transaction")
+//     }
 
-    revalidatePath("/")
-    redirect("/")
-}
+//     revalidatePath("/")
+//     redirect("/")
+// }
