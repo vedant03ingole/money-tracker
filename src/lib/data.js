8a77e3326f733ae9ad7a1e938e6d013de1b18504
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 20:18:54 +0530

    schema and mongo connection added

---
+++ src/lib/data.js
@@ -0,0 +1,13 @@
+import { Transaction } from "./schema";
+import { connectToDB } from './utils'
+
+export const fetchTransactions = async () => {
+    try{
+        connectToDB()
+        const transactions = await Transaction.find()
+        return transactions
+    } catch(err){
+        console.log(err)
+        throw new Error('Failed to fetch')
+    }
+}
---
+++ src/lib/utils.js
@@ -0,0 +1,15 @@
+import mongoose, { connection } from "mongoose"
+
+export const connectToDB = async () => {
+    const connection = {}
+
+    try{
+        if(connection.isConnected) return
+        const db = await mongoose.connect(process.env.MONGO)
+        connection.isConnected = db.connections[0].readyState
+        console.log('connected to db')
+    } catch (error) {
+        console.log(error);
+        throw new Error(error)
+    }
+}
