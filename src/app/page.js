f210fba6403d14ffb0b631ad426c0eb1b79d6155
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 21:36:24 +0530

    second rechecking

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ export default async function Home() {
     <>
       <div>Hello world</div>
       <div>
-        <h1>All Transactiosn</h1>
-        {transactions.map((transaction) => (
+        <h1>All Transactions</h1>
+        {transactions && transactions.map((transaction) => (
           <div key={transaction.id}>
             <h1>{transaction.category}</h1>
             <h1>{transaction.id}</h1>
--- src/lib/data.js
+++ src/lib/data.js
@@ -6,6 +6,7 @@ export const fetchTransactions = async () => {
         connectToDB()
         const transactions = await Transaction.find()
         return transactions
+        
     } catch(err){
         console.log(err)
         throw new Error('Failed to fetch')
