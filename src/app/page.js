adf04460ffb42e75b4fd33a56ae76cf432ecca88
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 22:26:37 +0530

    redeploy by adding env cariable

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-// import { fetchTransactions } from '../lib/data' 
+import { fetchTransactions } from '../lib/data' 
 import { connectToDB } from "@/lib/utils";
 
 export default async function Home() {
 
-  // const transactions = await fetchTransactions()
+  const transactions = await fetchTransactions()
   connectToDB()
   
   return (
@@ -11,12 +11,12 @@ export default async function Home() {
       <div>Hello world</div>
       <div>
         <h1>All Transactions</h1>
-        {/* {transactions && transactions.map((transaction) => (
+        {transactions && transactions.map((transaction) => (
           <div key={transaction.id}>
             <h1>{transaction.category}</h1>
             <h1>{transaction.id}</h1>
           </div>
-        ))} */}
+        ))}
         <br />
       </div>
     </>
