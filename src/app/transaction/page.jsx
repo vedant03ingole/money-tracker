0dd595d05b2b9d9a6198a24b5cae27d80fdeb4d3
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 21:09:27 +0530

    transaction form added

--- src/app/transaction/page.jsx
+++ src/app/transaction/page.jsx
@@ -1,63 +1,69 @@
-// import { addTransaction } from '@/lib/actions'
+import { addTransaction } from '@/lib/actions'
 
-// const AddTransaction = () => {
-//     return (
-//         <>
-//             <div>AddTransaction</div>
-//             <div className="text-black">
-//                 <form action={addTransaction}>
+const Transaction =  () => {
 
-//                     {/* Transaction type */}
-//                     <select name="transactionType" defaultValue="transactionType" required>
-//                         <option value="transactionType" disabled hidden>Transaction type</option>
-//                         <option value="income">Income</option>
-//                         <option value="expense">Expense</option>
-//                         <option value="transfer">Transfer</option>
-//                     </select>
+  const handleSubmit = async (formData) => {
+    "use server"
+    console.log(formData)
+  }
+    return (
+        <>
+            <div>Transaction</div>
+            <div className="text-black">
+                {/* <form action={addTransaction}> */}
+                <form action={handleSubmit}>
 
-//                     {/* Category */}
-//                     <select name="category" defaultValue="category" required>
-//                         <option value="category" disabled hidden>Transaction Category</option>
-//                         <option value="rent">Rent</option>
-//                         <option value="mess">Mess</option>
-//                         <option value="petrol">Petrol</option>
-//                         <option value="food">Food</option>
-//                         <option value="other">Other expenses</option>
-//                     </select>
+                    {/* Transaction type */}
+                    <select name="transactionType" defaultValue="transactionType" required>
+                        <option value="transactionType" disabled hidden>Transaction type</option>
+                        <option value="income">Income</option>
+                        <option value="expense">Expense</option>
+                        <option value="transfer">Transfer</option>
+                    </select>
 
-//                     {/* Transaction Amount */}
-//                     <input type="number" name="transactionAmount" required />
+                    {/* Category */}
+                    <select name="category" defaultValue="category" required>
+                        <option value="category" disabled hidden>Transaction Category</option>
+                        <option value="rent">Rent</option>
+                        <option value="mess">Mess</option>
+                        <option value="petrol">Petrol</option>
+                        <option value="food">Food</option>
+                        <option value="other">Other expenses</option>
+                    </select>
 
-//                     {/* Patment Method */}
-//                     <select name="paymentMethod" defaultValue="paymentMethod">
-//                         <option value="paymentMethod" disabled hidden>Payment method</option>
-//                         <option value="online">Online</option>
-//                         <option value="cash">Cash</option>
-//                     </select>
+                    {/* Transaction Amount */}
+                    <input type="number" name="transactionAmount" required />
 
-//                     {/* transaction date */}
-//                     <input type="date" name="transactionDate" required />
+                    {/* Patment Method */}
+                    <select name="paymentMethod" defaultValue="paymentMethod">
+                        <option value="paymentMethod" disabled hidden>Payment method</option>
+                        <option value="online">Online</option>
+                        <option value="cash">Cash</option>
+                    </select>
 
-//                     {/* Description */}
-//                     <textarea name="description" >Description</textarea>
+                    {/* transaction date */}
+                    <input type="date" name="transactionDate" required />
 
-
-//                     <button className="text-white" type="submit">Send</button>
-//                 </form>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default AddTransaction
+                    {/* Description */}
+                    <textarea name="description" >Description</textarea>
 
 
-import React from 'react'
-
-const Transaction = () => {
-  return (
-    <div>Transaction</div>
-  )
+                    <button className="text-white" type="submit">Send</button>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default Transaction
+
+
+// import React from 'react'
+
+// const Transaction = () => {
+//   return (
+//     <div>Transaction</div>
+//   )
+// }
+
+// export default Transaction
