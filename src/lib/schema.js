a642a2cc9dccc951c4cd711ba9628eb8d05481b6
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 21:13:01 +0530

    schema added

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -1,33 +1,33 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const transactionSchema = new mongoose.Schema({
-//     transactionType: {
-//         type: String,
-//         enum: ["income", "expense", "transfer"],
-//         required: true
-//     },
-//     category:{
-//         type: String,
-//         enum:['rent', 'mess', 'petrol', 'food', 'other']
-//     },
-//     transactionAmount:{
-//         type: Number,
-//         required: true
-//     },
-//     paymentMethod:{
-//         type: String,
-//         enum:["online", "cash"],
-//         required: true
-//     },
-//     transactionDate: {
-//         type: Date,
-//         required:true
-//         },
-//         description:{
-//         type: String
-//     }
-// },
-// { timestamps: true }
-// )
+const transactionSchema = new mongoose.Schema({
+    transactionType: {
+        type: String,
+        enum: ["income", "expense", "transfer"],
+        required: true
+    },
+    category:{
+        type: String,
+        enum:['rent', 'mess', 'petrol', 'food', 'other']
+    },
+    transactionAmount:{
+        type: Number,
+        required: true
+    },
+    paymentMethod:{
+        type: String,
+        enum:["online", "cash"],
+        required: true
+    },
+    transactionDate: {
+        type: Date,
+        required:true
+        },
+        description:{
+        type: String
+    }
+},
+{ timestamps: true }
+)
 
-// export const Transaction = mongoose.models.Transaction || mongoose.model("Transaction", transactionSchema)
+export const Transaction = mongoose.models.Transaction || mongoose.model("Transaction", transactionSchema)
