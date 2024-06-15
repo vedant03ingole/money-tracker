c1ae6dc7d74ad69a3c8ebd442e7b10f2519f38c6
Vedant Ingole <ivedant.30@gmail.com>
2024-06-15 21:44:24 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,22 @@
-import { fetchTransactions } from '../lib/data' 
+// import { fetchTransactions } from '../lib/data' 
 import { connectToDB } from "@/lib/utils";
 
 export default async function Home() {
 
-  const transactions = await fetchTransactions()
-  await connectToDB()
+  // const transactions = await fetchTransactions()
+  connectToDB()
   
   return (
     <>
       <div>Hello world</div>
       <div>
         <h1>All Transactions</h1>
-        {transactions && transactions.map((transaction) => (
+        {/* {transactions && transactions.map((transaction) => (
           <div key={transaction.id}>
             <h1>{transaction.category}</h1>
             <h1>{transaction.id}</h1>
           </div>
-        ))}
+        ))} */}
         <br />
       </div>
     </>
